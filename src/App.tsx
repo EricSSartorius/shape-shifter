8fe24ddb23eedddd7f78d08d98f81528d9d3baa6
EricSSartorius <EricSSartorius@gmail.com>
2022-10-06 16:07:47 -0700

    Fix auth style

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
-import styled from "styled-components";
-import { Logo } from "./components/Logo";
-import { Spinner } from "./components/Spinner";
-import { useGlobal } from "./context";
-import Auth from "./layout/Auth";
-import { DeviceLayout } from "./layout/DeviceLayout";
-import { ElementSettings } from "./layout/ElementSettings";
-import { LayoutSettings } from "./layout/LayoutSettings";
+import { AnimatePresence } from "framer-motion"
+import styled from "styled-components"
+import { Logo } from "./components/Logo"
+import { Spinner } from "./components/Spinner"
+import { useGlobal } from "./context"
+import Auth from "./layout/Auth"
+import { DeviceLayout } from "./layout/DeviceLayout"
+import { ElementSettings } from "./layout/ElementSettings"
+import { LayoutSettings } from "./layout/LayoutSettings"
 
 function App() {
-  const { session, layouts } = useGlobal();
+  const { session, layouts } = useGlobal()
 
-  if (!session && !layouts) return <Spinner />;
+  if (!session && !layouts) return <Spinner />
 
   return (
     <Main>
-      {!session ? (
-        <Auth />
-      ) : (
-        <>
-          <LayoutSettings />
-          <DeviceLayout />
-          <ElementSettings />
-        </>
-      )}
+      <AnimatePresence>
+        {!session ? (
+          <Auth />
+        ) : (
+          <>
+            <LayoutSettings />
+            <DeviceLayout />
+            <ElementSettings />
+          </>
+        )}
+      </AnimatePresence>
       <Logo />
     </Main>
-  );
+  )
 }
 
-export default App;
+export default App
 
 const Main = styled.main`
   max-width: 1290px;
@@ -45,4 +48,4 @@ const Main = styled.main`
     right: var(--gap-large);
     opacity: 0.2;
   }
-`;
+`
--- src/layout/Auth.tsx
+++ src/layout/Auth.tsx
@@ -1,52 +1,57 @@
-import styled from "styled-components";
-import { useState } from "react";
-import { supabase } from "../supabaseClient";
-import { Card } from "../components/Card";
+import styled from "styled-components"
+import { useState } from "react"
+import { supabase } from "../supabaseClient"
+import { Card } from "../components/Card"
+import { AnimatePresence, motion } from "framer-motion"
 
 export default function Auth() {
-  const [loading, setLoading] = useState(false);
-  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false)
+  const [email, setEmail] = useState("")
 
   const handleLogin = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     try {
-      setLoading(true);
-      const { error } = await supabase.auth.signInWithOtp({ email });
-      if (error) throw error;
-      alert("Check your email for the login link!");
+      setLoading(true)
+      const { error } = await supabase.auth.signInWithOtp({ email })
+      if (error) throw error
+      alert("Check your email for the login link!")
     } catch (error) {
-      alert(error.error_description || error.message);
+      alert(error.error_description || error.message)
     } finally {
-      setLoading(false);
+      setLoading(false)
     }
-  };
+  }
 
   return (
-    <AuthWrapper>
+    <AuthWrapper exit={{ opacity: 0, y: 20 }} initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }}>
       <h1>Sign In to Begin</h1>
-      <p className="description">
-        Sign in via magic link with your email below
-      </p>
-      {loading ? (
-        "Sending magic link..."
-      ) : (
-        <form onSubmit={handleLogin}>
-          <label htmlFor="email">
-            <span>Email</span>
-            <input
-              id="email"
-              type="email"
-              placeholder="Your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </label>
-          <button aria-live="polite">Send magic link</button>
-        </form>
-      )}
+      <p className="description">Sign in via magic link with your email below</p>
+      <AnimatePresence>
+        {loading ? (
+          <motion.h3 exit={{ opacity: 0 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+            Sending magic link...
+          </motion.h3>
+        ) : (
+          <motion.div exit={{ opacity: 0 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+            <form onSubmit={handleLogin}>
+              <label htmlFor="email">
+                <span>Email</span>
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="Your email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </label>
+              <button aria-live="polite">Send magic link</button>
+            </form>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </AuthWrapper>
-  );
+  )
 }
 
 const AuthWrapper = styled(Card)`
@@ -54,7 +59,18 @@ const AuthWrapper = styled(Card)`
   flex-direction: column;
   padding: var(--gap-large);
 
-  p {
+  h1,
+  .description,
+  h3 {
+    text-align: center;
+  }
+
+  h3 {
+    margin-top: var(--gap);
+    color: var(--violet);
+  }
+
+  .description {
     color: var(--grey);
     margin-top: 4px;
     font-size: var(--font-size-small);
@@ -86,4 +102,4 @@ const AuthWrapper = styled(Card)`
       background-position: right center;
     }
   }
-`;
+`
